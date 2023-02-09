d5ed2eef43ae852c3d678b1b47c7063905160ec5
Jon Harrison <jonathan_harrison@hotmail.com>
2023-02-09 12:01:20 +0000

    update App with new components and page structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
-import Nav from './pages/Nav.js';
-import Home from './pages/Home.js';
-import About from './pages/About.js';
-import Contact from './pages/Contact.js';
-import PageNotFound from './pages/PageNotFound.js';
-import Footer from './pages/Footer.js';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+import Main from './containers/Main';
+
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Portfolio from './pages/Portfolio';
+import PageNotFound from './pages/PageNotFound';
 
 const App = () => {
 
   return (
     <div className="App">
-      <Nav />
-      <Routes>
-        <Route path="/" element={<Home />} errorElement={<PageNotFound />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
+      <Header />
+      <Main>
+        <Routes>
+          <Route path="/" element={<Home />} errorElement={<PageNotFound />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </Main>
       <Footer />
     </div>
   );
