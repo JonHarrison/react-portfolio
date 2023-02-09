c1d5962f29ea28ee61c997dec2435a456febdb73
Jon Harrison <jonathan_harrison@hotmail.com>
2023-02-09 11:53:20 +0000

    index.js - include bootstrap.min.css

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter as Router } from 'react-router-dom';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './index.css';
 
 import App from './App';
+
 import reportWebVitals from './reportWebVitals';
 
 const container = document.getElementById('root');
