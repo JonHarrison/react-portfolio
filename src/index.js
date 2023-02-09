40f0d5af26279e728a726a23c409904a542680a5
Jon Harrison <jonathan_harrison@hotmail.com>
2023-02-09 18:00:35 +0000

    index.js - add bootstrap.min.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter as Router } from 'react-router-dom';
 
+import 'bootstrap/dist/js/bootstrap.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './index.css';
