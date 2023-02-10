25b57446c1785f02a9af81671de15235626a9c98
Jon Harrison <jonathan_harrison@hotmail.com>
2023-02-10 11:53:52 +0000

    update ProjectCard layout and style

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/card';
 import Button from 'react-bootstrap/button';
 import Carousel from 'react-bootstrap/carousel'
@@ -21,21 +22,21 @@ import './style.css';
 const ProjectCard = ({ project }) => {
     const { id, title, category, url, git, image, description } = project;
     return (
-        <div className="project-item col d-flex align-items-stretch">
+        <Col className="col project-item d-flex align-items-stretch">
             <Card className="card mb-3 bg-dark text-white" >
                 {/* <Card.Header>{title}</Card.Header> */}
                 <Card.Img className="img-container project-image img img-fluid rounded-start" variant="top" src={image} alt={title} />
                 {/* <Card.ImgOverlay> */}
-                    <Card.Body>
-                        <Card.Title>{title}</Card.Title>
-                        <Card.Text as="h6">{description}</Card.Text>
-                        {/* <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <Card.Body>
+                    <Card.Title>{title}</Card.Title>
+                    <Card.Text as="h6">{description}</Card.Text>
+                    {/* <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                             <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p> */}
-                        <div className="project-cta">
-                            <a href={git} className='btn'><FaGithub /></a>
-                            <a href={url} className='btn btn-primary' target='_blank'><FaLink /></a>
-                        </div>
-                    </Card.Body>
+                </Card.Body>
+                <div className="project-cta">
+                    <a href={git} className='btn'><FaGithub /></a>
+                    <a href={url} className='btn btn-primary' target='_blank'><FaLink /></a>
+                </div>
                 {/* </Card.ImgOverlay> */}
             </Card>
 
@@ -65,44 +66,44 @@ const ProjectCard = ({ project }) => {
                 <div className="content">
                     <h5>{title}</h5>
                     <h6>{category}</h6> */}
-                    {/* <i className="fa-brands fa-github" /> */}
-                    {/* <FontAwesomeIcon icon="fa-solid fa-check-square" /> */}
-                    {/* <FontAwesomeIcon icon="fa-brands fa-font-awesome" /> */}
-                    {/* <FontAwesomeIcon icon="fa-brands fa-github" /> */}
-                    {/* <h6>{description}</h6> */}
-                    {/* <FontAwesomeIcon icon="fa-brand fa-github" /> */}
-                    {/* <div>
+            {/* <i className="fa-brands fa-github" /> */}
+            {/* <FontAwesomeIcon icon="fa-solid fa-check-square" /> */}
+            {/* <FontAwesomeIcon icon="fa-brands fa-font-awesome" /> */}
+            {/* <FontAwesomeIcon icon="fa-brands fa-github" /> */}
+            {/* <h6>{description}</h6> */}
+            {/* <FontAwesomeIcon icon="fa-brand fa-github" /> */}
+            {/* <div>
                         <a href={git}><FaGithub /> Code Repository</a>
                     </div> */}
-                    {/* <div>
+            {/* <div>
                         <a href={url}><FaLink /> Live Website</a>
                     </div> */}
-                    {/* <Link
+            {/* <Link
                     to={git}
                     className="inline-flex"
                 >
                     <FaGithub />
                 </Link> */}
-                    {/* <Link
+            {/* <Link
                     to={`/projects/${id}`}
                     // bg-black text-white
                     className="inline-flex"
                 > */}
-                    {/* <span>Learn more</span> */}
-                    {/* <div> */}
-                    {/* <i className="fa-brands fa-github" /> */}
-                    {/* <FaGithub /> */}
-                    {/* <FontAwesomeIcon icon={brands('github')} /> */}
-                    {/* </div> */}
-                    {/* <span> */}
-                    {/* <i className="fa-brands fa-github" /> */}
-                    {/* <FaGithub /> */}
-                    {/* URL */}
-                    {/* </span> */}
-                    {/* </Link> */}
-                {/* </div> */}
+            {/* <span>Learn more</span> */}
+            {/* <div> */}
+            {/* <i className="fa-brands fa-github" /> */}
+            {/* <FaGithub /> */}
+            {/* <FontAwesomeIcon icon={brands('github')} /> */}
             {/* </div> */}
-        </div>
+            {/* <span> */}
+            {/* <i className="fa-brands fa-github" /> */}
+            {/* <FaGithub /> */}
+            {/* URL */}
+            {/* </span> */}
+            {/* </Link> */}
+            {/* </div> */}
+            {/* </div> */}
+        </Col>
 
     );
 }
